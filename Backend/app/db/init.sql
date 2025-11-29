04ee9301cf8189153d42fa75b5645719a96ad0dd
Luis-Golac <luis.golac@utec.edu.pe>
2025-11-28 19:26:25 -0500

    feat: db

---
+++ Backend/app/db/init.sql
@@ -0,0 +1,5 @@
+INSERT INTO device(password_device) VALUES('123');
+
+INSERT INTO sensor(sensor_id, model, name) VALUES(1, 'DHT22', 'temperatura');
+INSERT INTO sensor(sensor_id, model, name) VALUES(2, 'DHT22', 'humedad');
+INSERT INTO sensor(sensor_id, model, name) VALUES(3, 'LDR', 'luz');
