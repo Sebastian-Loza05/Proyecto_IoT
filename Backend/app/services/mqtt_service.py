2c5e17db3134154b3e0e331e605f67b3c3df9751
Luis-Golac <luis.golac@utec.edu.pe>
2025-11-28 19:35:01 -0500

    fix: guardaddo

--- Backend/app/services/mqtt_service.py
+++ Backend/app/services/mqtt_service.py
@@ -8,7 +8,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from sqlalchemy import select
 from datetime import datetime
-from db.models import Sensor, DeviceSensor
+from app.db.models import Sensor, DeviceSensor
+from app.db.database import AsyncSessionLocal
 
 async def save_sensor_data(db: AsyncSession, device_id: int, data: dict):
     now = datetime.now()
@@ -34,41 +35,41 @@ async def save_sensor_data(db: AsyncSession, device_id: int, data: dict):
             db.add(sensor_entry)
     await db.commit()
 
-async def mqtt_listener(
-    db: AsyncSession = Depends(get_db)
-):
-    while True:
-        try:
-            async with aiomqtt.Client(settings.MQTT_BROKER, settings.MQTT_PORT) as client:
-                await client.subscribe("invernadero/sensores")
-                async for message in client.messages:
-                    payload = message.payload.decode()
-                    print(f"Recibido: {payload}")
-                    try:
-                        # 1. Convertir string a Diccionario Python
-                        data = json.loads(payload)
 
-                        # 2. Imprimir valores individuales
-                        temp = data.get("temperatura")
-                        hum = data.get("humedad")
-                        luz = data.get("luz")
+async def mqtt_listener():
+    async with AsyncSessionLocal() as db:
+        while True:
+            try:
+                async with aiomqtt.Client(settings.MQTT_BROKER, settings.MQTT_PORT) as client:
+                    await client.subscribe("invernadero/sensores")
+                    async for message in client.messages:
+                        payload = message.payload.decode()
+                        print(f"Recibido: {payload}")
+                        try:
+                            # 1. Convertir string a Diccionario Python
+                            data = json.loads(payload)
 
-                        print(f" ➤ Temperatura procesada: {temp} °C")
-                        print(f" ➤ Humedad procesada: {hum} %")
-                        print(f" ➤ Luz procesada: {luz}")
+                            # 2. Imprimir valores individuales
+                            temp = data.get("temperatura")
+                            hum = data.get("humedad")
+                            luz = data.get("luz")
 
-                        device_id = data.get("device_id", 1)
+                            print(f" ➤ Temperatura procesada: {temp} °C")
+                            print(f" ➤ Humedad procesada: {hum} %")
+                            print(f" ➤ Luz procesada: {luz}")
 
-                        await save_sensor_data(db, device_id, data)
-                    # data = json.loads(payload)
+                            device_id = data.get("device_id", 1)
 
-                    # Guadar la data en la base de datos
+                            await save_sensor_data(db, device_id, data)
+                        # data = json.loads(payload)
 
-                    # Enviar a la App Móvil en tiempo real
-                        await manager.broadcast(payload)
-                    except json.JSONDecodeError:
-                        print(" [Error] JSON inválido recibido")
+                        # Guadar la data en la base de datos
 
-        except Exception as e:
-            print(f"Error en MQTT: {e}. Reintentando en 5s...")
-            await asyncio.sleep(5)
+                        # Enviar a la App Móvil en tiempo real
+                            await manager.broadcast(payload)
+                        except json.JSONDecodeError:
+                            print(" [Error] JSON inválido recibido")
+
+            except Exception as e:
+                print(f"Error en MQTT: {e}. Reintentando en 5s...")
+                await asyncio.sleep(5)
